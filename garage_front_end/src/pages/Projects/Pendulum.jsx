24f830d6ff70cc7c55f506996689978431ac276e
Panagiotis Anagnostaras <anagnostaras.p@gmail.com>
2024-12-27 17:07:23 +0200

    front end changes for more inputs

--- garage_front_end/src/pages/Projects/Pendulum.jsx
+++ garage_front_end/src/pages/Projects/Pendulum.jsx
@@ -3,13 +3,16 @@ import pendulum from "../../images/pendulum.png";
 import React, { useState } from 'react';
 
 let Pendulum = () => {
-    const [simData, setSimData] = useState("");
+    const [simSteps, setSimSteps] = useState("");
+    const [intTimeStep, setIntTimeStep] = useState("");
+    const [pendPos, setPendPos] = useState("");
+    const [pendVel, setPendVel] = useState("");
     const [responseData, setResponseData] = useState("");
     const [successfulPost, setSuccessfulPost] = useState(true);
 
     const sendData = async () => {
-        const data = { "simulation_steps": simData };
-        console.log(data);
+        const data = { "simulation_steps": simSteps, "integration_time_step": intTimeStep, "pendulum_position" : pendPos, "pendulum_velocity": pendVel};
+        console.log(data)
         try {
             const response = await fetch('http://localhost:4000/api/v1/simulation/', {
                 method: 'POST',
@@ -40,15 +43,55 @@ let Pendulum = () => {
             <a className='link-style' href="https://github.com/PanagiotisAnagnostaras/garage/tree/master/projects/inverted_pendulum" target="_blank" rel="noopener noreferrer">Code</a>
             <p>This project is about simulating a cart-pendulum system in C++.</p>
             <img src={pendulum} alt="pendulum"></img>
-            <h3>Run Simulation</h3>
-            <input
-                type="text"
-                value={simData}
-                onChange={(e) => setSimData(e.target.value)}
-                placeholder="Enter an int. The value doesn't matter for now."
-            />
-            <button onClick={sendData}>POST</button>
-            {successfulPost && responseData && <div><h4>Started simulation with {JSON.stringify(responseData.simulation_steps, null, 2)} steps</h4></div>}
+            <h3>Simulation Data</h3>
+            <div className="input-container">
+                <div className="input-item">
+                    <label htmlFor="input1" className="input-label">Number of simulation steps</label>
+                    <input
+                        id="input1"
+                        type="text"
+                        value={simSteps}
+                        onChange={(e) => setSimSteps(e.target.value)}
+                        placeholder="int"
+                        className="input-field"
+                    />
+                </div>
+                <div className="input-item">
+                    <label htmlFor="input2" className="input-label">Integration time step [sec]</label>
+                    <input
+                        id="input2"
+                        type="text"
+                        value={intTimeStep}
+                        onChange={(e) => setIntTimeStep(e.target.value)}
+                        placeholder="float"
+                        className="input-field"
+                    />
+                </div>
+                <div className="input-item">
+                    <label htmlFor="input3" className="input-label">Initial angle of pendulum [deg]</label>
+                    <input
+                        id="input3"
+                        type="text"
+                        value={pendPos}
+                        onChange={(e) => setPendPos(e.target.value)}
+                        placeholder="float"
+                        className="input-field"
+                    />
+                </div>
+                <div className="input-item">
+                    <label htmlFor="input4" className="input-label">Initial angular velocity of pendulum [deg/sec]</label>
+                    <input
+                        id="input4"
+                        type="text"
+                        value={pendVel}
+                        onChange={(e) => setPendVel(e.target.value)}
+                        placeholder="float"
+                        className="input-field"
+                    />
+                </div>
+                <button onClick={sendData}>Start</button>
+            </div>
+            {successfulPost && responseData && <div><h4>Started simulation</h4></div>}
             {!successfulPost && <div><h4>Failed to start simulation. Check your inputs.</h4></div>}
         </div>
     );
