b8d982d423f86966ccee025ed3447833b2c68727
Panagiotis Anagnostaras <anagnostaras.p@gmail.com>
2025-03-02 20:50:09 +0100

    fixed rewards bug

--- garage_back_end/projects/ppo/env.py
+++ garage_back_end/projects/ppo/env.py
@@ -3,24 +3,24 @@ from binder import Simulation
 from enum import Enum
 from typing import Union
 
-    
+
 class Env:
     class ObsIndexes(Enum):
         CART_VEL = 0
         PEND_POS = 1
         PEND_VEL = 2
         PREV_ACT = 3
-        
+
     class Constraints:
         max_cart_vel = 0
         max_pend_vel = 0
         max_input = 20
-        
+
     class Dimensions:
         def __init__(self, actions_dims, observations_dims) -> None:
             self.actions_dims = actions_dims
             self.observations_dims = observations_dims
-    
+
     def __init__(self, actions_dim: int, observations_dim: int, step_dt: float) -> None:
         self.dimensions = Env.Dimensions(actions_dims=actions_dim, observations_dims=observations_dim)
         self.step_dt = step_dt
@@ -33,7 +33,7 @@ class Env:
         self.sim.setPendVel(pend_vel)
         if cart_pos is not None:
             self.sim.setCartPos(cart_pos)
-    
+
     def get_observations(self) -> torch.Tensor:
         obs = torch.zeros(size=(self.dimensions.observations_dims,))
         obs[Env.ObsIndexes.CART_VEL.value] = self.sim.getCartVel()
@@ -41,16 +41,20 @@ class Env:
         obs[Env.ObsIndexes.PEND_VEL.value] = self.sim.getPendVel()
         obs[Env.ObsIndexes.PREV_ACT.value] = self.sim.getInput()
         return obs
-    
+
     def apply_actions(self, actions: torch.Tensor) -> None:
         self.sim.applyInput(actions.item())
         return
-    
+
     def get_reward(self, observations: torch.Tensor, actions: torch.Tensor) -> torch.Tensor:
         rew = torch.tensor(data=0, dtype=torch.float)
-        rew += torch.exp(-(observations[Env.ObsIndexes.PEND_POS.value])**2/1)
+        assert 0 <= observations[Env.ObsIndexes.PEND_POS.value] < 2 * torch.pi, "Angle must be in [0, 2pi)"
+        if observations[Env.ObsIndexes.PEND_POS.value] <= torch.pi:
+            rew += torch.exp(-((observations[Env.ObsIndexes.PEND_POS.value]) ** 2) / 1)
+        else:
+            rew += torch.exp(-((2 * torch.pi - observations[Env.ObsIndexes.PEND_POS.value]) ** 2) / 1)
         return rew
-    
+
     def step(self, actions: torch.Tensor) -> torch.Tensor:
         self.apply_actions(actions=actions)
         self.sim.run(self.step_dt, False)
